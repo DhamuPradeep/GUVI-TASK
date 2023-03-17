80fd631dd2a0955985c39e86e7689d6fde55f3ec
Dhamu Pradeep <73932175+DhamuPradeep@users.noreply.github.com>
2023-03-17 22:39:39 +0530

    Update login.php

--- php/login.php
+++ php/login.php
@@ -2,20 +2,27 @@
 $email = $_POST['email'];
 $password = $_POST['password'];
 
+$hostname = 'localhost';
+$username = 'root';
+$userpassword ='';
+$dbname = 'guvi';
+$con =  mysqli_connect($hostname,$username,$userpassword,$dbname);
+if(mysqli_connect_error()){
+    die(0);
+}
+
 if(isset($email) !== null && isset($password) !==null){
-    $hostname = 'localhost';
-    $username = 'root';
-    $userpassword ='';
-    $dbname = 'guvi';
-    $con = new mysqli($hostname,$username,$userpassword,$dbname);
-    if (!$con){
-        die(0);
-    }
-    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'"; 
-    $result = $connect->query($sql);
-    if ($result->num_rows > 0) {
-        echo "Login";
-      } else {
+    
+    $stmt = $con->prepare("SELECT * FROM users WHERE email=(?)and password=(?)");
+    $stmt->bind_param("ss",$email,$password);
+    $stmt->execute();
+    $result=$stmt->get_result();
+    $rows=$result->num_rows;
+    $stmt->close();
+    $con->close();
+    if ($rows==1) {
+        echo 'success';
+    } else {
         echo "Login failed";
     }
 }
