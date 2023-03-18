049e98fddf85b8fc6791784c18010035003308ca
Dhamu Pradeep <73932175+DhamuPradeep@users.noreply.github.com>
2023-03-18 22:43:20 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -2,8 +2,8 @@
 require '../assets/vendor/autoload.php';
 
 
+  $redis = new Redis();
   $email = $_POST['Email'];
-
     $uri = "mongodb+srv://dhamupradeep:4405iezdbjNP39e9@cluster0.mtzek67.mongodb.net/?retryWrites=true&w=majority";
     $manager = new MongoDB\Client($uri);
     $collection = $manager->guvi->Users;
