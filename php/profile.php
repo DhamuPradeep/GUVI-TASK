54afa44f065caff33a5741c6d8478b1528e09edf
Dhamu Pradeep <73932175+DhamuPradeep@users.noreply.github.com>
2023-03-18 22:37:29 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,3 +1,27 @@
 <?php
+require '../assets/vendor/autoload.php';
+
+
+  $email = $_POST['Email'];
+
+    $uri = "mongodb+srv://dhamupradeep:4405iezdbjNP39e9@cluster0.mtzek67.mongodb.net/?retryWrites=true&w=majority";
+    $manager = new MongoDB\Client($uri);
+    $collection = $manager->guvi->Users;
+   
+    $cursor = $collection->find([
+      'email' => $email
+    ]);
+
+    $data = array();
+
+    foreach ($cursor as $document) {
+        $data[] = $document;
+    }
+
+    $json =  json_encode($data);
+
+    echo $json;
+
+    exit;
 
 ?>
