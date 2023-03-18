8379f20b823c9d1947c5b5bcd96f746f86a2887d
Dhamu Pradeep <73932175+DhamuPradeep@users.noreply.github.com>
2023-03-18 22:37:55 +0530

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,4 +1,7 @@
 <?php
+
+require '../assets/vendor/autoload.php';
+
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $email = $_POST['email'];
@@ -21,12 +24,27 @@ if(isset($email) !== null && isset($password) !==null && isset($firstName) != nu
     $rows=$stmt->num_rows;
     if($rows > 0){
         echo "User Already Exist";
+        $stmt->close();
+        $con->close();
         die(0);
     }else{
         $stmt->close();
         $stmt=$con->prepare("INSERT Into users(firstname,lastname,email,phonenumber,password) values(?,?,?,?,?)");
         $stmt->bind_param("sssss",$firstName,$lastName,$email,$phonenumber,$password);
         $stmt->execute();
+      //  $uri ='mongodb+srv://dhamupradeep2610:4405iezdbjNP39e9@cluster0.ojymx1j.mongodb.net/?retryWrites=true&w=majority';
+        $uri = "mongodb+srv://dhamupradeep:4405iezdbjNP39e9@cluster0.mtzek67.mongodb.net/?retryWrites=true&w=majority";
+        $manager = new MongoDB\Client($uri);
+        // $database = "guvi";
+        // $collection = "users";
+        $collections = $manager->guvi->users;
+        $insertOneResult = $collections->insertOne([
+            'email' => $email,
+            'phone' => $phonenumber,
+            'firstname'=> $firstName,
+            'lastname' => $lastName,
+            'password' => $password
+        ]);
         echo "User Details Added Successfully";
     }
     $stmt->close();
